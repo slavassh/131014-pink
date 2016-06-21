026acb8e0076bf16e8317d33885df8f5f725934d
Viacheslav Shestakov <slavassh@gmail.com>
2016-06-21 17:31:03 +0500

    Начал оптимизацию автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,23 @@ var precss = require("precss");
 var inlinesvg = require("postcss-inline-svg");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var csso = require('gulp-csso');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require("gulp-rename");
+var imagemin = require('gulp-imagemin');
+var svgstore = require('gulp-svgstore');
 
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(postcss([
       precss(),
       inlinesvg(),
+      mqpacker({
+        sort: true
+      }),
       autoprefixer({browsers: [
         "last 1 version",
         "last 2 Chrome versions",
@@ -23,8 +33,31 @@ gulp.task("style", function() {
       ]})
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("css"))
     .pipe(server.reload({stream: true}));
 });
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+        .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest('build/img'))
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("img/*.svg")
+        .pipe(svgstore({
+          inlineSvg: true
+        }))
+        .pipe(rename("symbols.svg"))
+        .pipe(gulp.dest("build/img"));
+});
+
 
 gulp.task("serve", ["style"], function() {
   server.init({
